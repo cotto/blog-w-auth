8885337b09d42d627e38f5e492e13ce64b935677
Christoph Otto <christoph_github@mksig.org>
2011-09-14 19:26:18 -0700

    initial commit - fresh new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlogWAuth::Application.config.secret_token = 'a98005fd9fe94242cf76c9eafe19c641da6697e65c6757a1e2783388ced01cb7ae7cd6538dfdb182c9322ec73dffcf0cf95c82d4cb57704c783049f7e3f9d5ca'
